7df97ae1545329a11d297982b5a0b91bae180f73
Richard LaBrecque <RichardlaBreque@gmail.com>
2022-04-25 11:20:55 -0400

    feat: controller exists

---
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe 'munchies requests' do
+  it 'has a successful response' do
+    get '/api/v1/munchies?start=denver,co&destination=pueblo,co&food=chinese'
+    binding.pry
+  end
+end
