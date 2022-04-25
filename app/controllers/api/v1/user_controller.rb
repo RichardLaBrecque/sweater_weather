94df0087e201b8dfd60ed73c41c974aebb0bb42c
Richard LaBrecque <RichardlaBreque@gmail.com>
2022-04-25 17:07:38 -0400

    feat: user_register

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::UserController < ApplicationController
   def create
     if params[:password] != params[:password_confirmation]
-      render json: { data: { message: 'passwords do not match' } }
+      render json: { data: { message: 'passwords do not match' } }, status: :bad_request
     elsif User.find_by(email: params[:email])
-      render json: { data: { message: 'that email already exists' } }
+      render json: { data: { message: 'that email already exists' } }, status: :bad_request
     else
       if  user = User.create(user_params)
           render json: UserSerializer.new(user)
